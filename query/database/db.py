4677ca09aa0c0cf42847ac80a2897bf8effa9ef7
ian5030560 <110732376+ian5030560@users.noreply.github.com>
2023-06-13 22:42:29 +0800

    change again

--- query/database/db.py
+++ query/database/db.py
@@ -3,7 +3,7 @@ import pymysql as sql
 __host = "140.127.74.226"
 __user = "411077016"
 __password = "411077016"
-__db = "411077019"
+__db = "411077016"
 
 __connect = sql.connect(host = __host, user = __user, password = __password, database = __db)
 
